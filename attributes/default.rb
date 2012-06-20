ac4ed5431dd5352b4bf6b97021207b0033803ab3
Avishai Ish-Shalom <avishai@fewbytes.com>
2012-06-20 14:36:14 +0300

    Fixed kafka attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
-default[:kafka][:tarball_url] = "http://fewbytes-clients.s3.amazonaws.com/conduit/kafka-0.7.0-compiled.tar.gz"
-default[:kafka][:tarball_checksum] = "88b3b68f581d9c53c6ade52f70207114cceac368ff2200ac036a73d3efa0ef7f"
+default[:kafka][:tarball_url] = "http://people.apache.org/~nehanarkhede/kafka-0.7.0-incubating/kafka-0.7.0-incubating-src.tar.gz"
+default[:kafka][:tarball_checksum] = "08b6732080af7d063d2fde5358c5b02eaa5b3e9e376d71fb99de33d7cf5d5115"
 default[:kafka][:extracted_dir] = "kafka-0.7.0-compiled"
-default[:kafka][:brokerid] = fqdn.hash.abs
+default[:kafka][:brokerid] = fqdn.hash.abs % 2 ** 31
 default[:kafka][:zk_cluster_name] = "default"
 default[:kafka][:number_of_partitions_per_topic] = 1
