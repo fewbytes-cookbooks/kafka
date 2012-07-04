d87d70c91919834ca3a7be5222e1c341be1526f2
Avishai Ish-Shalom <avishai@fewbytes.com>
2012-07-04 16:05:44 +0300

    Don't regenerate brokerid

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
 default[:kafka][:archive_url] = "http://public.fewbytes.com/kafka-0.7.0.zip"
 default[:kafka][:archive_checksum] = "accc4f96d3e42cb0ec6eb532ed62b12172a22c7b222a0070bf78b576f6b9a22a"
-default[:kafka][:brokerid] = fqdn.hash.abs % 2 ** 31
+default_unless[:kafka][:brokerid] = fqdn.hash.abs % 2 ** 31
 default[:kafka][:zk_cluster_name] = "default"
 default[:kafka][:number_of_partitions_per_topic] = 1
 default["kafka"]["config_dir"] = "/etc/kafka"
