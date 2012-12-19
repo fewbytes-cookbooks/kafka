b63c4e94110a136d2c632864395478cd4235fdb8
Avishai Ish-Shalom <avishai@fewbytes.com>
2012-12-19 15:54:27 +0200

    kafka/zookeeper fixes

--- recipes/default.rb
+++ recipes/default.rb
@@ -13,6 +13,8 @@ user "kafka" do
   system true
 end
 
+package "unzip"
+
 kafka_archive = node[:kafka][:archive_url][/.*\/([^\/]+)$/,1] # => Retrieve last part of the URL
 remote_file ::File.join("/tmp/", kafka_archive) do
 	source node[:kafka][:archive_url]
