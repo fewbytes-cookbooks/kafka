99d0dd17c95d096592cf5b2f3b6d06d3320a9bd7
Yoni Yalovitsky <yoni@fewbytes.com>
2013-03-20 16:03:50 +0200

    fixed kafka brokerid to return positive signed int

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,10 +1,10 @@
 require 'zlib'
 
-default[:kafka][:archive_url] = "http://public.fewbytes.com/kafka-0.7.1.zip"
-default[:kafka][:archive_checksum] = "e71386369d5960bd62da7b88b393b5f8b98838ee33b4683175b40f062cfe3aee"
-default[:kafka][:brokerid] = Zlib.crc32(fqdn)
-default[:kafka][:zk_cluster_name] = "default"
-default[:kafka][:number_of_partitions_per_topic] = 1
+default["kafka"]["archive_url"] = "http://public.fewbytes.com/kafka-0.7.1.zip"
+default["kafka"]["archive_checksum"] = "e71386369d5960bd62da7b88b393b5f8b98838ee33b4683175b40f062cfe3aee"
+default["kafka"]["brokerid"] = (Zlib.crc32(fqdn) % 2**31) #this needs to enter a Java int
+default["kafka"]["zk_cluster_name"] = "default"
+default["kafka"]["number_of_partitions_per_topic"] = 1
 default["kafka"]["config_dir"] = "/etc/kafka"
 default["kafka"]["base_dir"] = "/opt/kafka"
 default["kafka"]["jmx_port"] = 9999
