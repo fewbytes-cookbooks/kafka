4b3fec5b6edbab066dfd7a9823153a5e5c194e98
Avishai Ish-Shalom <avishai@fewbytes.com>
2014-05-15 17:36:16 +0300

    Fix missing variables in service recipe

--- recipes/service.rb
+++ recipes/service.rb
@@ -5,11 +5,11 @@ include_recipe "kafka::install"
 runit_service "kafka" do
   options({
     :log_dir => node[:kafka][:log_dir],
-    :install_dir => install_dir,
-    :java_home => java_home,
+    :install_dir => node[:kafka][:install_dir],
+    :java_home => node['java']['java_home'],
     :log_dir => node[:kafka][:log_dir],
     :java_jmx_port => node[:kafka][:jmx_port],
     :java_class => "kafka.Kafka",
-    :user => user
+    :user => node[:kafka][:user]
   })
 end
